746c4f44f40f77569d3f588692098a9d6120b180
Ty Eggen <tycho.eggen@gmail.com>
2023-10-23 06:36:54 +0000

    tools: updated example tools

--- docker-openswoole-example.sh
+++ docker-openswoole-example.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+EXAMPLE=openswoole
+
+CONF="$(pwd)/docker/${EXAMPLE}.conf"
+CMD=/colore/examples/${EXAMPLE}/server.php
+
+if [ $# -gt 0 ]; then
+    CMD="$@"
+fi
+
 docker run \
     -it \
     --rm \
     -p 9501:9501 \
     -v $(pwd):/colore \
     openswoole/swoole \
-    /colore/examples/openswoole/server.php
+    ${CMD}
--- docker-ping-example.sh
+++ docker-ping-example.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+EXAMPLE=ping
+
+CONF="$(pwd)/docker/${EXAMPLE}.conf"
+CMD=""
+
+if [ $# -gt 0 ]; then
+    CMD="$@"
+fi
+
 docker run \
     -it \
     --rm \
     -p 8765:80 \
     -v $(pwd):/colore \
-    -v $(pwd)/docker/docker-php.conf:/etc/apache2/conf-available/docker-php.conf \
-    -v $(pwd)/docker/000-default.conf:/etc/apache2/sites-enabled/000-default.conf \
+    -v $(pwd)/docker/apache-colore.conf:/etc/apache2/conf-available/docker-php.conf \
+    -v ${CONF}:/etc/apache2/sites-enabled/000-default.conf \
     php:8-apache \
-    $@
+    ${CMD}
